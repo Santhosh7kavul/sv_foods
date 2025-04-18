fca9f5b4a2fdb66743a8daa46b19938a3f810fc7
Santhosh7kavul <73489703+Santhosh7kavul@users.noreply.github.com>
2025-04-18 10:31:14 +0530

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cornrichsilage.in/',
+    generateRobotsTxt: true, // also generates robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+    outDir: 'public',
+  }
